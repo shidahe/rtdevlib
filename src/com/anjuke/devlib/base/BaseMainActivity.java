459d9a7d5d0a6df0dbed294966aa8da343335d19
rarnu <rarnu1985@gmail.com>
2013-03-21 20:52:19 +0800

    clean project

--- src/com/anjuke/devlib/base/BaseMainActivity.java
+++ src/com/anjuke/devlib/base/BaseMainActivity.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
