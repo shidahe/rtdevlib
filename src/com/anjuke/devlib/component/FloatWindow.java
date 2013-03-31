47c7f5f0cf739bbb62a5a7a7f7b1c83c8f29e1b2
rarnu <rarnu1985@gmail.com>
2013-03-31 13:02:04 +0800

    fix the bug when drag the float window

--- src/com/anjuke/devlib/component/FloatWindow.java
+++ src/com/anjuke/devlib/component/FloatWindow.java
@@ -9,13 +9,11 @@ import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 
 public class FloatWindow extends View implements OnTouchListener {
+	@SuppressWarnings("unused")
 	private Context context;
 	private WindowManager windowMgr;
 	private WindowManager.LayoutParams wmParams;
 	private View view;
-	private int tag = 0;
-	private int oldOffsetX;
-	private int oldOffsetY;
 	private float lastX, lastY;
 
 	public FloatWindow(Context context, View view) {
@@ -65,34 +63,24 @@ public class FloatWindow extends View implements OnTouchListener {
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
-		final int action = event.getAction();
 
-		float x = event.getX();
-		float y = event.getY();
+		float x = event.getRawX();
+		float y = event.getRawY();
 
-		if (tag == 0) {
-			oldOffsetX = wmParams.x;
-			oldOffsetY = wmParams.y;
-		}
-
-		if (action == MotionEvent.ACTION_DOWN) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
 			lastX = x;
 			lastY = y;
-
-		} else if (action == MotionEvent.ACTION_MOVE) {
+			break;
+		case MotionEvent.ACTION_MOVE:
 			wmParams.x += (int) (x - lastX);
 			wmParams.y += (int) (y - lastY);
-			tag = 1;
 			windowMgr.updateViewLayout(view, wmParams);
+			lastX = x;
+			lastY = y;
+			break;
 		}
 
-		else if (action == MotionEvent.ACTION_UP) {
-			int newOffsetX = wmParams.x;
-			int newOffsetY = wmParams.y;
-			if (oldOffsetX != newOffsetX || oldOffsetY != newOffsetY) {
-				tag = 0;
-			}
-		}
 		return true;
 	}
 
