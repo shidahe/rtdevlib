d53689c9ef6382aa412e06e415e3a521bfddd676
rarnu <rarnu1985@gmail.com>
2013-04-08 18:51:32 +0800

    add tab fragment

--- src/com/anjuke/devlib/base/BaseFragment.java
+++ src/com/anjuke/devlib/base/BaseFragment.java
@@ -4,5 +4,13 @@ import com.anjuke.devlib.base.inner.InnerFragment;
 
 
 public abstract class BaseFragment extends InnerFragment {
+	
+	public BaseFragment() {
+		super();
+	}
+	
+	public BaseFragment(String tagText, String tabTitle) {
+		super(tagText, tabTitle);
+	}
 
 }
--- src/com/anjuke/devlib/base/BasePopupFragment.java
+++ src/com/anjuke/devlib/base/BasePopupFragment.java
@@ -3,5 +3,11 @@ package com.anjuke.devlib.base;
 import com.anjuke.devlib.base.inner.InnerFragment;
 
 public abstract class BasePopupFragment extends InnerFragment {
+	public BasePopupFragment() {
+		super();
+	}
 
+	public BasePopupFragment(String tagText, String tabTitle) {
+		super(tagText, tabTitle);
+	}
 }
