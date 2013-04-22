080331e439bbd801c2ad5eeacedd022d90c930ec
rarnu <rarnu1985@gmail.com>
2013-04-22 16:42:10 +0800

    fix the bug when re-set ViewPager's adapter with fragments

--- samples/src/com/rarnu/devlib/demo/fragment/TabFragment.java
+++ samples/src/com/rarnu/devlib/demo/fragment/TabFragment.java
@@ -3,11 +3,11 @@ package com.rarnu.devlib.demo.fragment;
 import java.util.List;
 import java.util.Random;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.anjuke.devlib.base.BaseFragment;
 import com.anjuke.devlib.base.BaseTabFragment;
 import com.rarnu.devlib.demo.MainActivity;
 import com.rarnu.devlib.demo.R;
@@ -15,16 +15,6 @@ import com.rarnu.devlib.demo.R;
 public class TabFragment extends BaseTabFragment {
 
 	MenuItem itemAdd, itemRemove;
-	
-	@Override
-	public void initFragmentList(List<BaseFragment> listFragment) {
-		listFragment.add(new TabViewFragment("Fragment 1", "Tab 1"));
-		listFragment.add(new TabViewFragment("Fragment 2", "Tab 2"));
-		listFragment.add(new TabViewFragment("Fragment 3", "Tab 3"));
-		listFragment.add(new TabViewFragment("Fragment 4", "Tab 4"));
-		listFragment.add(new TabViewFragment("Fragment 5", "Tab 5"));
-
-	}
 
 	@Override
 	protected int getBarTitle() {
@@ -68,6 +58,16 @@ public class TabFragment extends BaseTabFragment {
 	@Override
 	protected void onGetNewArguments(Bundle bn) {
 
+	}
+
+	@Override
+	public void initFragmentList(List<Fragment> listFragment) {
+		listFragment.add(new TabViewFragment("Fragment 1", "Tab 1"));
+		listFragment.add(new TabViewFragment("Fragment 2", "Tab 2"));
+		listFragment.add(new TabViewFragment("Fragment 3", "Tab 3"));
+		listFragment.add(new TabViewFragment("Fragment 4", "Tab 4"));
+		listFragment.add(new TabViewFragment("Fragment 5", "Tab 5"));
+		
 	}
 
 }
