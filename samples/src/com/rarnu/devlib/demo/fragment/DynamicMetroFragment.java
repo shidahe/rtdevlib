cc2b5c30b5c3d67039ca00d1a5c0d248fb926aea
rarnu <rarnu1985@gmail.com>
2013-06-18 18:18:04 +0800

    add click event for mergeView

--- samples/src/com/rarnu/devlib/demo/fragment/DynamicMetroFragment.java
+++ samples/src/com/rarnu/devlib/demo/fragment/DynamicMetroFragment.java
@@ -6,9 +6,11 @@ import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.anjuke.devlib.base.BaseFragment;
 import com.anjuke.devlib.component.BlockView.FocusCallback;
+import com.anjuke.devlib.component.BlockView.ItemClickListener;
 import com.anjuke.devlib.component.HScrollLayout;
 import com.anjuke.devlib.component.MergeView;
 import com.anjuke.devlib.component.tools.MergePage;
@@ -16,7 +18,8 @@ import com.anjuke.devlib.utils.FileUtils;
 import com.anjuke.devlib.utils.UIUtils;
 import com.rarnu.devlib.demo.R;
 
-public class DynamicMetroFragment extends BaseFragment implements FocusCallback {
+public class DynamicMetroFragment extends BaseFragment implements
+		FocusCallback, ItemClickListener {
 
 	MergeView mvPage1, mvPage2, mvPage3;
 	MergePage mp1, mp2, mp3;
@@ -47,7 +50,8 @@ public class DynamicMetroFragment extends BaseFragment implements FocusCallback 
 		mvPage2 = (MergeView) innerView.findViewById(R.id.mvPage2);
 		mvPage3 = (MergeView) innerView.findViewById(R.id.mvPage3);
 
-		// the main activity is not landscape, so here needs to swap the width and height value;
+		// the main activity is not landscape, so here needs to swap the width
+		// and height value;
 		mvPage1.setSize(UIUtils.getHeight(), UIUtils.getWidth());
 		mvPage2.setSize(UIUtils.getHeight(), UIUtils.getWidth());
 		mvPage3.setSize(UIUtils.getHeight(), UIUtils.getWidth());
@@ -82,13 +86,13 @@ public class DynamicMetroFragment extends BaseFragment implements FocusCallback 
 
 		mvPage1.setViews(mp1.getViews(getActivity(),
 				R.layout.item_dynamic_cell, new String[] { "image", "text" },
-				new int[] { R.id.ivImage, R.id.tvItem }, options, this));
+				new int[] { R.id.ivImage, R.id.tvItem }, options, this), this);
 		mvPage2.setViews(mp2.getViews(getActivity(),
 				R.layout.item_dynamic_cell, new String[] { "image", "text" },
-				new int[] { R.id.ivImage, R.id.tvItem }, options, this));
+				new int[] { R.id.ivImage, R.id.tvItem }, options, this), this);
 		mvPage3.setViews(mp3.getViews(getActivity(),
 				R.layout.item_dynamic_cell, new String[] { "image", "text" },
-				new int[] { R.id.ivImage, R.id.tvItem }, options, this));
+				new int[] { R.id.ivImage, R.id.tvItem }, options, this), this);
 
 	}
 
@@ -134,5 +138,12 @@ public class DynamicMetroFragment extends BaseFragment implements FocusCallback 
 
 	public String getData(Context context, int index) throws IOException {
 		return FileUtils.readAssetFile(context, String.format("data%d", index));
+	}
+
+	@Override
+	public void onItemClick(int index) {
+		Toast.makeText(getActivity(), String.valueOf(index), Toast.LENGTH_SHORT)
+				.show();
+
 	}
 }
